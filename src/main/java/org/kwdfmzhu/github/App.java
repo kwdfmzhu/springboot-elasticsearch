722773278e632d4344755668ea4c46bd895eaa67
zhukewei <zhukewei@treefinance.com.cn>
2017-07-05 09:32:35 +0800

    init project

--- src/main/java/org/kwdfmzhu/github/App.java
+++ src/main/java/org/kwdfmzhu/github/App.java
@@ -1,13 +1,19 @@
 package org.kwdfmzhu.github;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication(scanBasePackages = {"org.kwdfmzhu.github"},exclude={DataSourceAutoConfiguration.class})
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
